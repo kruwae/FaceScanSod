3bf9e29ed76e3eb108a7315661bbf64434c3d0fc
School Admin <admin@school.ac.th>
2026-04-13 16:13:28 +0700

    setup js

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxwhi7a8L9mLZ2MvoKgkalGj-ia5iRTZVYOuh3w24dKv4wQUz_3z0cALdRQv1NpclIL/exec';

b46323fea2b068663a48ffe29c12f8d34525a2e8
School Admin <admin@school.ac.th>
2026-04-14 16:24:24 +0700

    Check LoginAPI3

--- js/api-config.js
+++ js/api-config.js
@@ -2,7 +2,7 @@
 //  ตั้งค่า Google Apps Script Web App URL + Google OAuth Client ID
 //  แก้ไขค่าเหล่านี้ก่อนใช้งาน หากมีการ Deploy GAS ใหม่หรือเปลี่ยน OAuth
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwuA2mKe1HWrEihz8gKswHs5lajx6F-QsLK8xm-8uK7-0MfaSP1trmRoYGYv7MkjLiM/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwVK8I_N3UNb_etBPgT9PKfcmSN9NYR2Mu64fq-dR62sdlxaBnSfmaZFy44W9Ao4mYE/exec';
 const GOOGLE_OAUTH_CLIENT_ID = '938397636593-gstnelk2u35c6aa8hg0594ja0k40b5su.apps.googleusercontent.com';
 const GOOGLE_CLIENT_ID = GOOGLE_OAUTH_CLIENT_ID;
 

b098f7e34a3911cd6a458e48c8a1fb92f2ad8a04
kruwae <kruwae.arong@gmail.com>
2026-04-23 13:15:32 +0700

    SetAPI2

--- js/api-config.js
+++ js/api-config.js
@@ -2,7 +2,7 @@
 //  ตั้งค่า Google Apps Script Web App URL + Google OAuth Client ID
 //  แก้ไขค่าเหล่านี้ก่อนใช้งาน หากมีการ Deploy GAS ใหม่หรือเปลี่ยน OAuth
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxe8Ar-DVvcYHvkidB1jw8xZWCab4SWjZG1ULv8jEI5AzMYKyvL189jrsxbDERNjh8/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzccUDtobzi59Uvs4X1ZCpJ9VWVf8LaMLqrvqKDmzweudt663Ym3u7TGhc9GI2Fp9YN/exec';
 const GOOGLE_OAUTH_CLIENT_ID = '938397636593-gstnelk2u35c6aa8hg0594ja0k40b5su.apps.googleusercontent.com';
 const GOOGLE_CLIENT_ID = GOOGLE_OAUTH_CLIENT_ID;
 
